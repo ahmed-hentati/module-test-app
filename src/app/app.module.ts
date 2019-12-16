abf2547c5212b07a80340509adde1d33faa6b567
unknown <mister.bouhmid@gmail.com>
2019-12-16 04:07:19 +0100

    S1P8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrderListComponent } from './orders/order-list/order-list.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderListComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
